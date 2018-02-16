b96ebcd8be47c988834d3a64b5269044386afa08
Ross Hill <rosshill88@gmail.com>
2018-02-16 15:34:30 +0000

    Created test constructor

---
+++ hero.js
@@ -0,0 +1,5 @@
+const Hero = function(name) {
+  this.name = name;
+  this.health = health;
+  this.fav_food = fav_food;
+}
---
+++ specs/hero_spec.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const Hero = require('../hero');
+
+describe('Hero', function() {
+
+let hero1;
+let hero2;
+let hero3;
+let hero4;
+
+beforeEach(function() {
+  hero1 = new Hero("Fox");
+  hero2 = new Hero("Falco");
+  hero3 = new Hero("Peppy");
+  hero4 = new Hero("Slippy");
+})
+
+
+
+})
