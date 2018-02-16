c578a5c2f5dd9445fdb6f0f32234710777cdfb98
Ross Hill <rosshill88@gmail.com>
2018-02-16 15:58:09 +0000

    Hero can say name tested

--- hero.js
+++ hero.js
@@ -1,7 +1,11 @@
-const Hero = function(name, health) {
+const Hero = function(name, health, fav_food) {
   this.name = name;
   this.health = health;
-  // this.fav_food = fav_food;
+  this.fav_food = fav_food;
+}
+
+Hero.prototype.talk = function() {
+  return `My name's ${this.name}!`;
 }
 
 module.exports = Hero;
--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -9,10 +9,10 @@ let hero3;
 let hero4;
 
 beforeEach(function() {
-  hero1 = new Hero("Fox", 450);
-  hero2 = new Hero("Falco", 480);
-  hero3 = new Hero("Peppy", 320);
-  hero4 = new Hero("Slippy", 360);
+  hero1 = new Hero("Fox", 450, "berries");
+  hero2 = new Hero("Falco", 480, "worms");
+  hero3 = new Hero("Peppy", 320, "carrots");
+  hero4 = new Hero("Slippy", 360, "flies");
 })
 
 it('hero should have name', function() {
@@ -21,18 +21,18 @@ it('hero should have name', function() {
 });
 
 it('hero should have health', function() {
-  const actual = hero1.health;
-  assert.strictEqual(actual, 450);
+  const actual = hero2.health;
+  assert.strictEqual(actual, 480);
 });
 
-xit('hero should have fave food', function() {
-  const actual = hero1.fav_food;
-  assert.strictEqual(actual, "berries");
+it('hero should have fave food', function() {
+  const actual = hero3.fav_food;
+  assert.strictEqual(actual, "carrots");
 });
 
-xit('hero can talk', function() {
-  const actual = hero1.talk;
-  assert.strictEqual(actual, "All ships check in!!");
+it('hero can talk', function() {
+  const actual = hero4.talk();
+  assert.strictEqual(actual, "My name's Slippy!");
 });
 
 xit('hero should have a collection of tasks --empty', function() {
