2306bceadac713094747bf77f4a2350fff24b12e
Ross Hill <rosshill88@gmail.com>
2018-02-16 18:07:10 +0000

    Food has name and replenishment value

--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const Hero = require('../hero');
 const Task = require('../task');
+const Food = require('../food');
 
 describe('Hero', function() {
 
@@ -9,6 +10,9 @@ let hero2;
 let hero3;
 let hero4;
 let task1;
+let food1;
+let food2;
+let food3;
 
 beforeEach(function() {
   hero1 = new Hero("Fox", 450, "berries");
@@ -17,6 +21,9 @@ beforeEach(function() {
   hero4 = new Hero("Slippy", 360, "flies");
   task1 = new Task("Counter attack Venom", 1, 5, "booster parts");
   task2 = new Task("Destroy the rock crusher", 2, 8, "shield upgrade");
+  food1 = new Food("meat", 50);
+  food2 = new Food("berries", 35);
+  food3 = new Food("worms", 20);
 })
 
 it('hero should have name', function() {
@@ -85,6 +92,14 @@ it('task can be marked as completed', function() {
   const actual = hero1.tasklist[0].completed;
   // const actual = task1.completed;
   assert.strictEqual(actual, true);
+});
+
+it('food should have name', function() {
+  assert.strictEqual(food1.name, "meat");
+});
+
+it('food should have replenishment_value', function() {
+  assert.strictEqual(food2.replenishment_value, 35);
 });
 
 
